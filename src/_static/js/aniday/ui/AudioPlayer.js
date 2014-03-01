54082df71a4b1f62e2f01c4f92d01c9553c63025
PimDerks <info@pimderks.com>
2014-03-01 17:57:23 +0100

    Styling for audio player.

--- src/_static/js/aniday/ui/AudioPlayer.js
+++ src/_static/js/aniday/ui/AudioPlayer.js
@@ -1,4 +1,4 @@
-define(['conditioner','conditioner/Observer','audiojs','utils/Merge'],function(conditioner, Observer, Audio, Merge){
+define(['conditioner','conditioner/Observer','audiojs','utils/Merge', 'utils/Element'],function(conditioner, Observer, AudioJS, Merge, Element){
 
     'use strict';
 
@@ -9,46 +9,50 @@ define(['conditioner','conditioner/Observer','audiojs','utils/Merge'],function(c
 
         /*jshint multistr: true */
 
+        var title = element.innerHTML;
+
         // Options
         var defaultOptions = {
             css: false,
             swfLocation: '../' + 'audiojs.swf',
             createPlayer: {
                 markup: '\
-                <div class="play-pause"> \
-                    <button class="play" type="button">Play</button> \
-                    <button class="pause" type="button">Pause</button> \
-                    <p class="loading"></p> \
-                    <p class="error"></p> \
+                <div class="audiojs__play-pause"> \
+                    <button class="audiojs__play-pause__play" type="button"><span class="icon icon-play"></span><span class="screenreader">Afspelen</span></button> \
+                    <button class="audiojs__play-pause__pause" type="button"><span class="icon icon-pause"></span><span class="screenreader">Pauzeren</span></button> \
+                    <p class="audiojs__play-pause__loading"></p> \
+                    <p class="audiojs__play-pause__error"></p> \
                 </div> \
-                <div class="scrubber"> \
-                    <div class="progress"></div> \
-                    <div class="loaded"></div> \
+                <div class="audiojs__title">' + title + '</div><div class="audiojs__time"> \
+                    <span class="audiojs__time__played">00:00</span>/<span class="audiojs__time__duration">00:00</span> \
                 </div> \
-                <div class="time"> \
-                    <em class="played">00:00</em>/<strong class="duration">00:00</strong> \
-                </div> \
-                <div class="error-message"></div>',
-                playPauseClass: 'play-pause',
-                scrubberClass: 'scrubber',
-                progressClass: 'progress',
-                loaderClass: 'loaded',
-                timeClass: 'time',
-                durationClass: 'duration',
-                playedClass: 'played',
-                errorMessageClass: 'error-message',
-                playingClass: 'playing',
-                loadingClass: 'loading',
-                errorClass: 'error'
+                <div class="audiojs__scrubber"><div class="audiojs__scrubber__inner"> \
+                    <div class="audiojs__scrubber__progress"></div> \
+                    <div class="audiojs__scrubber__loaded"></div> \
+                </div></div> \
+                <div class="audiojs__error-message"></div>',
+                playPauseClass: 'audiojs__play-pause',
+                scrubberClass: 'audiojs__scrubber',
+                progressClass: 'audiojs__scrubber__progress',
+                loaderClass: 'audiojs__scrubber__loaded',
+                timeClass: 'audiojs__time',
+                durationClass: 'audiojs__time__duration',
+                playedClass: 'audiojs__time__played',
+                errorMessageClass: 'audiojs__error-message',
+                playingClass: 'audiojs__play-pause__playing',
+                loadingClass: 'audiojs__play-pause__loading',
+                errorClass: 'audiojs__error-message'
             },
-            play: this.play.bind(this),
-            playPause: this.playPause.bind(this),
-            pause: this.pause.bind(this)
+            play:this.play.bind(this),
+            pause:this.pause.bind(this)
         };
 
         this._options = Merge(this._options, defaultOptions, false);
 
-        this._initialize();
+        var _this = this;
+        AudioJS.audiojs.events.ready(function() {
+            _this._initialize();
+        });
 
     };
 
@@ -64,30 +68,67 @@ define(['conditioner','conditioner/Observer','audiojs','utils/Merge'],function(c
 
         _initialize: function () {
 
-            this._player = Audio.create(this._element, this._options);
+            // prevent clicks on anchor
+            this._onClickBind = this._onClick.bind(this);
+            this._element.addEventListener('click', this._onClickBind);
+
+            // create audio
+            this._audio = document.createElement('audio');
+
+            // set src
+            this._audio.src = this._element.href;
+
+            // append
+            Element.insertBefore(this._audio, this._element);
+
+            // create player
+            this._player = AudioJS.audiojs.create(this._audio, this._options);
+
+            // listen to clicks on player
+            this._player.wrapper.addEventListener('click', this._onClickPlayer.bind(this));
 
         },
 
-        _init: function(){
+        _onClick: function(e){
+
+            e.preventDefault();
+            e.stopPropagation();
+
+        },
+
+        _onClickPlayer: function(e){
+
+            var target = e.target;
+            // check if target is not a button
+            while(target.nodeName.toLowerCase() != 'button' && target.nodeName.toLowerCase() != 'li'){
+                target = target.parentNode;
+            }
+
+            if(target.nodeName.toLowerCase() != 'button'){
+                this._player.playPause();
+            }
 
         },
 
         load: function(src){
-            this._player.load(src);
+
         },
 
-        play: function(){
+        play: function() {
+            this._player.wrapper.classList.add('audiojs--playing');
+            Observer.publish(this, 'play', this);
         },
 
-        playPause: function(){
-        },
-
-        pause: function(){
-            // pim.element.removeClass(this._artwork,'playing');
+        pause: function() {
+            this._player.wrapper.classList.remove('audiojs--playing');
         },
 
         getPlayer: function(){
             return this._player;
+        },
+
+        stop: function(){
+            this._player.pause();
         },
 
         /**
@@ -99,6 +140,12 @@ define(['conditioner','conditioner/Observer','audiojs','utils/Merge'],function(c
          */
 
         unload:function(){
+
+            // stop listening
+            this._element.removeEventListener('click', this._onClickBind);
+
+            // remove node
+            Element.remove(this._audio);
 
         }
 
--- src/_static/js/aniday/ui/AudioPlaylist.js
+++ src/_static/js/aniday/ui/AudioPlaylist.js
@@ -7,12 +7,6 @@ define(['conditioner','conditioner/Observer','ui/AudioPlayer'],function(conditio
         this._element = element;
         this._options = options;
 
-        // Get reference to audio element
-        this._player = document.getElementById(this._element.getAttribute('data-audio-player'));
-
-        // Get anchors
-        this._anchors = this._element.querySelectorAll('a');
-
         // Initialize
         this._initialize();
 
@@ -30,202 +24,40 @@ define(['conditioner','conditioner/Observer','ui/AudioPlayer'],function(conditio
 
         _initialize: function () {
 
-            // Get elements
-            this._nodes = this._getNodes();
+            // get audio
+            this._audio = this._element.querySelectorAll('a');
 
-            // Array of files
-            this._files = [];
+            // array with players
+            this._players = [];
 
-            // Listen to clicks on anchors
-            var l = this._anchors.length, i = 0;
+            // create players
+            var l = this._audio.length,
+                i = 0;
             for(; i < l; i++){
-                var current = this._anchors[i];
-                this._files.push(current.getAttribute('href'));
-                current.addEventListener('click', this._onAnchorClick.bind(this));
+                var player = new AudioPlayer(this._audio[i], {});
+                this._players.push(player);
+                Observer.subscribe(player, 'play', this._onPlay.bind(this));
             }
-
-            var play = '<button class="play-pause play" type="button"><span class="icon icon-play"><span class="screenreade">Play</span></span></button>';
-            var next = '<button class="next" type="button"><span class="icon icon-forward"><span class="screenreade">Forward</span></span></button>';
-            var pause = '<button class="play-pause pause" type="button"><span class="icon icon-pause"><span class="screenreade">Pause</span></span></button>';
-            var prev = '<button class="prev" type="button"><span class="icon icon-backward"><span class="screenreade">Backward</span></span></button>';
-
-            // Initialize player
-            this._player = new AudioPlayer(this._player, {
-                createPlayer: {
-                    markup: '<ul class="controls"><li>' + prev + '</li><li>' + play + '</li><li>' + pause + '</li><li>' + next + '</li></ul><div class="status"><span class="loading"></span><span class="error"></span></div><div class="scrubber"><div class="progress"></div><div class="loaded"></div></div><div class="time"><em class="played">00:00</em>/<strong class="duration">00:00</strong></div><div class="error-message"></div>',
-                    playPauseClass: 'play-pause',
-                    scrubberClass: 'scrubber',
-                    progressClass: 'progress',
-                    loaderClass: 'loaded',
-                    timeClass: 'time',
-                    durationClass: 'duration',
-                    playedClass: 'played',
-                    errorMessageClass: 'error-message',
-                    playingClass: 'playing',
-                    loadingClass: 'loading',
-                    errorClass: 'error'
-                },
-                updatePlayhead: this._onUpdatePlayHead.bind(this)
-            });
-
-            // Get reference to player wrapper
-            this._audio = this._player.getPlayer();
-
-            // Get reference to time element
-            this._played = this._audio.wrapper.querySelector('.played');
-
-            // Get reference to progress in player
-            var player = this._audio.settings.createPlayer;
-            this._playerProgress = this._audio.wrapper.querySelector('.' + player.progressClass);
-
-            // Create prev
-            this._prevButton = this._audio.wrapper.querySelector('.prev');
-            this._prevButton.addEventListener('click', this._prev.bind(this));
-
-            // Create next
-            this._nextButton = this._audio.wrapper.querySelector('.next');
-            this._nextButton.addEventListener('click', this._next.bind(this));
-
-            // Index
-            this._index = 0;
-
-            // Load
-            this._load();
 
         },
 
-        _getNodes: function(){
-            var l = this._anchors.length,
+        _onPlay: function(player){
+
+            // currently playing
+            this._current = player;
+
+            // stop other players
+            var l = this._players.length,
                 i = 0,
-                result = [];
+                current;
+
             for(; i < l; i++){
-                var current = this._anchors[i];
-                var obj = {
-                    'anchor' : current,
-                    'item': current.parentNode,
-                    'progress': current.parentNode.querySelector('.song-progress')
-                };
-                result.push(obj);
-            }
-            return result;
-        },
-
-        _onAnchorClick: function(e){
-
-            // Stop event
-            e.preventDefault();
-            e.stopPropagation();
-
-            // Get mp3
-            var src = e.target,
-                l = this._anchors.length,
-                index = null,
-                i = 0;
-
-            // Get index
-            for(; i < l; i++){
-                if(this._anchors[i] == src){
-                    index = i;
+                current = this._players[i];
+                if(this._players[i] !== this._current){
+                    current.stop();
                 }
             }
 
-            // load
-            this._index = index;
-            this._load(index);
-            this._player.play();
-            this._audio.play();
-
-        },
-
-        _next: function(){
-            if(this._index !== this._files.length - 1){
-                this._index = this._index + 1;
-                this._load(this._index);
-            }
-            this._player.play();
-            this._audio.play();
-        },
-
-        _prev: function(){
-            if(this._index !== 0){
-                this._index = this._index - 1;
-                this._load(this._index);
-            }
-            this._player.play();
-            this._audio.play();
-        },
-
-        _load: function(index){
-
-            if(!index){
-                index = this._index;
-            }
-            this._audio.load(this._files[index]);
-            this._resetNodes();
-            this._update();
-        },
-
-        _onUpdatePlayHead: function(percent){
-
-            // update playtime
-            var p = this._audio.duration * percent;
-
-            // calculate current playtime
-            var m = Math.floor(p / 60),
-                s = Math.floor(p % 60);
-
-            this._played.innerHTML = (m < 10 ? '0' : '') + m + ':' + (s < 10 ? '0' : '') + s;
-
-            // update progress for player
-            this._playerProgress.style.width = (percent * 100) + '%';
-
-            // update progress for track
-            if(this._nodes){
-                var current = this._nodes[this._index];
-                if(current){
-                    current.progress.style.width = (percent * 100) + '%';
-                }
-            }
-
-        },
-
-        _update: function(){
-
-            // activate current item
-            this._resetNodes();
-            // pim.element.addClass(this._nodes[this._index].item, 'active');
-
-            // update controls
-            this._updateControls();
-
-        },
-
-        _updateControls: function(){
-            // hide/show prev
-            if(this._index === 0){
-                //pim.element.addClass(this._prevButton, 'disabled')
-            } else {
-                //pim.element.removeClass(this._prevButton, 'disabled');
-            }
-
-            // hide/show next
-            if(this._index == this._files.length - 1){
-                //pim.element.addClass(this._nextButton, 'disabled');
-            } else {
-                //pim.element.removeClass(this._nextButton, 'disabled');
-            }
-
-        },
-
-        _resetNodes: function(){
-            var l = this._nodes.length,
-                i = 0;
-
-            for(; i < l; i++){
-                var current = this._nodes[i];
-                // pim.element.removeClass(current.item, 'active');
-                current.progress.style.width = '';
-            }
         },
 
         /**
